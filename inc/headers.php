5ea9ef110f23c67fe976889964a6e0cb3c826ed4
JokuHenkilo <c1mais00@students.oamk.fi>
2023-03-20 12:41:07 +0200

    Functions and headers files

---
+++ inc/headers.php
@@ -0,0 +1,10 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
+
+if($_SERVER['REQUEST_METHOD'] === 'OPTIONS')
+{
+	return 0;
+}
