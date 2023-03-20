3e1aeaa8a5e80d57d73607709734f6e89e0e04be
JokuHenkilo <c1mais00@students.oamk.fi>
2023-03-20 12:35:40 +0200

    main file fix

--- index.php
+++ index.php
@@ -1,27 +1,17 @@
 <?php
-header('Access-Control-Allow-Origin: *');
-header('Content-Type: application/json');
-
-$server_name=”localhost”;
-
-$username=”root”;
-
-$password=””;
-
-$database_name=”database123″;
+require_once "inc/functions.php";
+require_once "inc/headers.php";
 
 try
 {
-   $db = new PDO('mysql:host=127.0.0.1;dbname=shoppingList;charset=utf8','root', 'root');
-   $sql = "select * from item";
-   $query = $db->query($sql);
-   $results = $query->fetchAll(PDO::FETCH_ASSOC);
-   header('HTTP/1.1 200 OK');
-   print json_encode($results);
+	$db = openDb();
+	$sql = "select * from item";
+	$query = $db->query($sql);
+	$results = $query->fetchAll(PDO::FETCH_ASSOC);
+	header("HTTP/1.1 200 OK");
+	print json_encode($results);
 }
 catch (PDOException $pdoex)
 {
-   print "<p>Database connection failed!".$pdoex->getMessage();
+	returnError($pdoex);
 }
-
-?>
